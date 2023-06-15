15c57acbd2ee8979929c1d0e4a941c4c3b1ed904
a-kumar5 <ayush.1306.kumar@gmail.com>
2023-06-15 10:49:28 +0530

    Actuator introduction

---
+++ src/main/java/com/techchaggi/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,29 @@
+package com.techchaggi.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    
+	// expose "/" that return app name
+	
+	@GetMapping("/")
+	public String TCMyCoolApp() {
+		return "Tech Chaggi My Cool App";
+	}
+	
+	// expose a new endpoint for "workout"
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run, Swim and lift some weight!";
+	}
+	
+	// expose new endpoint for login page
+	
+	@GetMapping("/login")
+	public String login() {
+		return "Login Page!";
+	}
+}
